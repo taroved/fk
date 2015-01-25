f2a36036e999f434740d8ea38191efd36870cfbb
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2015-01-25 18:45:18 +0200

    Added db

--- core/models.py
+++ core/models.py
@@ -2,10 +2,47 @@ from django.db import models
 
 from wagtail.wagtailcore.models import Page
 
+class AboutPage(Page):
+    pass
+
+class AccreditationPage(Page):
+    pass
+
+class ArchivePage(Page):
+    pass
+
+class BussinesPage(Page):
+    pass
 
 class HomePage(Page):
     pass
 
+class MaterialsPage(Page):
+    pass
+
+class NewsPage(Page):
+    pass
+
+class OrgPage(Page):
+    pass
+
+class PanelPage(Page):
+    pass
+
+class ParticipationPage(Page):
+    pass
+
+class ProgramPage(Page):
+    pass
+
+class RadaPage(Page):
+    pass
+
+class SpeakerPage(Page):
+    pass
+
+class SpeakersPage(Page):
+    pass
 
 class EmptyPage(Page):
     pass
