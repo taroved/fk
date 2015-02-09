353b0f62abf7381bed0e03b9d0d6d67702d7922c
Alex Golikov <golikov.aleksey@gmail.com>
2015-02-09 17:18:49 +0200

    new port

--- rebuild-and-restart.sh
+++ rebuild-and-restart.sh
@@ -4,6 +4,6 @@ cd /root/forumkyiv.org
 docker build --force-rm --rm -t forumkyiv .
 docker stop forumkyiv.org
 docker rm forumkyiv.org
-docker run -d --name forumkyiv.org -p 8001:8000 forumkyiv
+docker run -d --name forumkyiv.org -p 8002:8000 forumkyiv
 cd /root
 ./restart-nginx.sh
