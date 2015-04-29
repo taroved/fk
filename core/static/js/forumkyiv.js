87c419d72c16c409a0760ec02eb01033de483094
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2015-04-29 23:33:12 +0300

    Close #200

---
+++ core/static/js/forumkyiv.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    function clorize(element, color1, color2) {
+        var c = $(element).attr("class");
+        var locationSelector = ".section-location ." + c;
+        //var timeSelector = ".section-time ." + c;
+
+        $(locationSelector).css("color", color1);
+        //$(timeSelector).css("color",color1);
+        $(element).css("color", color2);
+
+        console.log(selector);
+    }
+
+
+    $(".section-name ul li").mouseover(function () {
+        clorize(this, "#0098cf", "#0098cf");
+    });
+
+    $(".section-name ul li").mouseout(function () {
+        clorize(this, "#989ca2", "#000");
+    });
+});
